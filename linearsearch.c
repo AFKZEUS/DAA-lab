68b76f585c5ba4c869d119c5b5f281f593c8b9d8
AFKZEUS <53431124+AFKZEUS@users.noreply.github.com>
2021-09-03 20:04:14 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cout<<"Enter the number of elements in array"<<endl;
+    cin<<n;
+    int a[n];
+    for(i=0;i<n;i++){
+        cout<<a[i]<<endl;
+        }
+}
