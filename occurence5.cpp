8fb8cb84d2b3110bc6e2717522bf3d2d8838197a
AFKZEUS <53431124+AFKZEUS@users.noreply.github.com>
2021-09-03 20:04:29 +0530

    Add files via upload

---
+++ occurence5.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int m,n;
+	cin>>m;
+	int a[m];
+	for(int i=0; i<m; i++)
+	cin>>a[i];
+	cin>>n;
+	int b[n];
+	for(int i=0; i<n; i++)
+	cin>>b[i];
+	sort(a,a+m);
+	sort(b,b+n);
+	int i=0,j=0;
+	while(i<m && j<n)
+	{
+	 	if(a[i]==b[j])
+	 	{
+	 		cout<<a[i]<<" ";
+	 		i++;
+	 		j++;
+		}
+		else if(a[i]>b[j])
+		j++;
+		else
+		i++;
+	}
+
+}
